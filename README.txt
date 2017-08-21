5a90663d183a3546bfa614c07860384f7b6d991c
Shiva Pun <shivapun@Shivas-iMac.local>
2017-08-21 15:50:17 +0100

    project4 first commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/stphn/pen/OPEJPj.
+
+ ##A basic responsive navigation.
+This is a mix of two pens I needed for a project
+
+Some Credits goes to:
+
+[http://codepen.io/ErickFlorez/pen/cujmI](http://codepen.io/ErickFlorez/pen/cujmI)
+[http://codepen.io/fantasticthinking/pen/ogwKOp](http://codepen.io/fantasticthinking/pen/ogwKOp)
---
+++ js/scroll.js
@@ -0,0 +1,48 @@
+// optional script using jQuery
+
+// fixed navbar on scroll
+var stickyNavTop=$('.fixed-nav-bar').offset().top;
+$(window).on('scroll', function(){
+  if($(window).scrollTop()+$('.fixed-nav-bar').height()>
+  stickyNavTop-1) {
+    $('.fixed-nav-bar').addclass('.fixed-nav-bar');
+  } else {
+   $('.fixed-nav-bar').removeClass('.fixed-nav-bar'); 
+  }
+});
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
